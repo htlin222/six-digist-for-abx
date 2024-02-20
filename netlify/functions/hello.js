977d5e42f46fc79e6ad1808ebe61e69969e8ac5e
Hsieh-Ting Lin (林協霆) <61491860+htlin222@users.noreply.github.com>
2024-02-20 13:45:57 +0800

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,15 +1,25 @@
-// hello there!
-// 
-// I'm a serverless function that you can deploy as part of your site.
-// I'll get deployed to AWS Lambda, but you don't need to know that. 
-// You can develop and deploy serverless functions right here as part
-// of your site. Netlify Functions will handle the rest for you.
-
-
 exports.handler = async event => {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-        statusCode: 200,
-        body: `Hello ${subject}!`,
-    }
-}
+  // 獲取當前 UTC 時間
+  const now = new Date();
+  
+  // 轉換為台北時區時間，台北時區為 UTC+8
+  const taipeiTime = new Date(now.getTime() + 8 * 60 * 60 * 1000);
+  
+  // 計算月份和日期的和
+  const dateSum = taipeiTime.getMonth() + 1 + taipeiTime.getDate();
+  
+  // 獲取當前小時並計算和
+  const hour = taipeiTime.getHours();
+  const hourSum = Math.floor(hour / 10) + (hour % 10);
+  
+  // 生成一個0到99之間的隨機數
+  const randomNum = Math.floor(Math.random() * 100);
+  
+  // 格式化輸出字符串
+  const result = `${dateSum.toString().padStart(2, '0')}${hourSum.toString().padStart(2, '0')}${randomNum.toString().padStart(2, '0')}`;
+  
+  return {
+    statusCode: 200,
+    body: result,
+  };
+};
